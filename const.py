c3fc3def3befddde671a913ba85d6c3b89e69c6e
Huynh Hung <84199166+mineloop99@users.noreply.github.com>
2023-12-13 18:30:11 +0700

    update mouse

---
+++ const.py
@@ -0,0 +1,4 @@
+AUTO_STARTED_LOG = "Auto started!"
+STARTED_LOG = "Started!"
+AUTO_STOPPED_LOG = "Auto stopped!"
+STOPPED_LOG = "Stopped!"
--- pro_auto_find_pokemon.py
+++ pro_auto_find_pokemon.py
@@ -1,9 +1,13 @@
 import sys
+from threading import Thread
 import keyboard
+import mouse
 import time
 import random
 import tkinter as tk
 import tkinter.messagebox as msgbox
+
+from const import AUTO_STARTED_LOG, AUTO_STOPPED_LOG, STARTED_LOG, STOPPED_LOG
 
 
 def show_message_pop_up(message, timeout=500):
@@ -14,8 +18,8 @@ def show_message_pop_up(message, timeout=500):
 
 
 key_quit_program = "q"
-key_start = "-"
-key_start_with_run_away = "`"
+key_start = "`"
+key_start_with_run_away = "-"
 key_stop = "="  # Must diff key start
 
 time_beetween_press = 0.1  # in second
@@ -32,12 +36,12 @@ def on_auto_press_no_run():
         return
     else:
         on_running = True
-    print("Auto started!")
-    show_message_pop_up("Started!")
+    print(AUTO_STARTED_LOG)
+    show_message_pop_up(STARTED_LOG)
     while on_running:
         if keyboard.is_pressed(key_stop):
-            print("Auto stopped!")
-            show_message_pop_up("stopped!")
+            print(AUTO_STOPPED_LOG)
+            show_message_pop_up(STOPPED_LOG)
             on_running = False
             return
         random_number = random.randint(0, 1)
@@ -56,12 +60,38 @@ def on_auto_press_with_run():
         return
     else:
         on_running = True
-    print("Auto started!")
-    show_message_pop_up("Started!")
+    print(AUTO_STARTED_LOG)
+    show_message_pop_up(STARTED_LOG)
     while on_running:
         if keyboard.is_pressed(key_stop):
-            print("Auto stopped!")
-            show_message_pop_up("stopped!")
+            print(AUTO_STOPPED_LOG)
+            show_message_pop_up(STOPPED_LOG)
+            on_running = False
+            return
+        random_number = random.randint(0, 1)
+        keyboard.press("4")
+        keyboard.release("4")
+        if random_number == 0:
+            keyboard.press(key_auto_move[0])
+        elif random_number == 1:
+            keyboard.press(key_auto_move[1])
+        time.sleep(time_beetween_press)
+        keyboard.release(key_auto_move[0])
+        keyboard.release(key_auto_move[1])
+
+
+def on_auto_mouse_press_with_run():
+    global on_running
+    if on_running:
+        return
+    else:
+        on_running = True
+    print(AUTO_STARTED_LOG)
+    show_message_pop_up(STARTED_LOG)
+    while on_running:
+        if mouse.is_pressed(mouse.LEFT):
+            print(AUTO_STOPPED_LOG)
+            show_message_pop_up(STOPPED_LOG)
             on_running = False
             return
         random_number = random.randint(0, 1)
@@ -83,5 +113,10 @@ def exit_program():
 
 keyboard.add_hotkey(key_start, on_auto_press_no_run)
 keyboard.add_hotkey(key_start_with_run_away, on_auto_press_with_run)
-keyboard.add_hotkey(key_quit_program, exit_program)
+mouse.on_button(on_auto_mouse_press_with_run, (), mouse.X, "down")
+thread_mouse = Thread(target=mouse.wait)
+print("Mouse listen is starting!")
+thread_mouse.daemon = True
+thread_mouse.start()
+print("Keyboard listen is starting!")
 keyboard.wait()
