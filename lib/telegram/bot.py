d3ac8cd4f1214bef8c5f078df2087ac42462cad5
Huynh Hung <84199166+mineloop99@users.noreply.github.com>
2023-12-18 10:07:58 +0700

    fix tele on off

--- lib/telegram/bot.py
+++ lib/telegram/bot.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 import telegram
 
-from lib.telegram.config import BOT_CATCHING_TELEGRAM_API, USER_TELEGRAM_ID
+from lib.telegram.config import BOT_CATCHING_TELEGRAM_API, USER_TELEGRAM_ID, TELEGRAM_ON
 
 isSending = False
 
 
 async def send_message(text: str):
+    if TELEGRAM_ON != "True":
+        return
     global isSending
     isSending = True
     if BOT_CATCHING_TELEGRAM_API == None:
--- lib/telegram/config.py
+++ lib/telegram/config.py
@@ -5,3 +5,4 @@ import os
 load_dotenv()
 BOT_CATCHING_TELEGRAM_API = os.getenv("BOT_CATCHING_TELEGRAM_API")
 USER_TELEGRAM_ID = os.getenv("USER_TELEGRAM_ID")
+TELEGRAM_ON = os.getenv("TELEGRAM_ON")
